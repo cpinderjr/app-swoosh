087fdb92d90430754273038b0ea2a38114248e96
Carlos Pinder <carlos.pinder@icloud.com>
2019-08-02 10:01:14 -0400

    Added the Player model to store data changes when buttons are pressed. Setup the LeagueVC buttons (Mens, Womens, Co-ed) to activate the Next button.

---
+++ app-swoosh/app-swoosh/Model/Player.swift
@@ -0,0 +1,15 @@
+//
+//  Player.swift
+//  app-swoosh
+//
+//  Created by Carlos Pinder on 8/2/19.
+//  Copyright © 2019 Carlos Pinder. All rights reserved.
+//
+
+import Foundation
+
+struct Player {
+    var desiredLeague: String?
+    var selectedSkillLevel: String?
+    
+}
